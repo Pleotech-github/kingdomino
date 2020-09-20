8fa9d34c7465ec508a2e197cfdbf6c78b26d43ba
TheGr8Chibi <43129699+TheGr8Chibi@users.noreply.github.com>
2020-09-20 17:39:36 +0200

    i tried

--- king_domino/main.py
+++ king_domino/main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-board = cv2.imread('2.jpg')
+board = cv2.imread('1.jpg')
 hsv = cv2.cvtColor(board, cv2.COLOR_BGR2HSV)
 gray = cv2.cvtColor(board, cv2.COLOR_BGR2GRAY)
 w, h = board.shape[::-2]
@@ -43,31 +43,104 @@ upperGR = np.array([30, 145, 160])
 GRmask = cv2.inRange(hsv, lowerGR, upperGR)
 GRres = cv2.bitwise_and(board, board, mask=GRmask)
 
+rows, cols = (5,5)
+cardArray = [[0]*cols]*rows
+
 greenCards = []
 blueCards = []
 darkGreenCards = []
 yellowCards = []
 
-def rasterize(picture, cardAr):
-    cards = []
+class Score:
+    def __init__(self, card, score):
+        self.card = card
+        self.score = score
+
+cards = []
+
+def countScore2(color, cardAr):
+    groupMax = 1
+    scoreArray = np.zeros((5, 5))
+    first = True
+    sorted = False
+    for i in range(0,5):
+        for j in range(0,5):
+            if cardAr[i][j] != 0:
+                if first == True:
+                    scoreArray[i][j] = 1
+                    first = False
+                    continue
+                sorted = False
+                for a in range(0,5):
+                    for b in range(0,5):
+                        if cardAr[a][b] != 0:
+                            #print("a-i: " + str(abs(a - i)))
+                            #print("b-j:" + str(abs(b-j)))
+                            if a-i == 0 and abs(b-j) == 1:
+                                if scoreArray[a][b] == 0:
+                                    scoreArray[i][j] = groupMax
+                                    scoreArray[a][b] = groupMax
+                                    print("PLUSA" + str(groupMax))
+                                else:
+                                    scoreArray[i][j] = scoreArray[a][b]
+                                sorted = True
+                                print("A")
+                                break
+                            elif b-j == 0 and abs(a-i) == 1:
+                                if scoreArray[a][b] == 0:
+                                    scoreArray[i][j] = groupMax
+                                    scoreArray[a][b] = groupMax
+                                    print("PLUSB")
+                                else:
+                                    scoreArray[i][j] = scoreArray[a][b]
+                                    print("ELSE")
+                                sorted = True
+                                print("B")
+                                break
+                            else:
+                                groupMax+=1
+                                print("END")
+                if sorted == False:
+                    print("D")
+                    print("a-i: " + str(abs(a - i)))
+                    print("b-j:" + str(abs(b-j)))
+                    groupMax += 1
+                    scoreArray[i][j] = groupMax
+    print(scoreArray)
+
+
+cardArray1 = np.zeros((5,5))
+
+def rasterize(picture, cardAr, color):
+
+    sc = Score(color,0)
+
     for i in range(1, 6):
         for j in range(1, 6):
             card = picture[(j - 1) * 100:(j - 1) * 100 + 100, (i - 1) * 100:(i - 1) * 100 + 100]
             cards.append(card)
 
-    for i in range(0, 25):
-        result = cv2.mean(cards[i])
-        if result[1] > 10.0:
-            cardAr.append(cards[i])
-            cv2.imshow("Card " + str(i + 1), cards[i])
+            result = cv2.mean(card)
+            if result[1] > 10.0:
+                cardArray1[i - 1][j - 1] = 1
+                #cardAr.append(cards[i])
+                cv2.imshow("Card " + str(i + 1), card)
+    print(cardArray1)
+    countScore2("b", cardArray1)
 
-#rasterize(LGres, greenCards)
+
+
+                    #tempAr = np.array([group,i-1,j-1])
+                    #scoreAr = np.append(scoreAr, tempAr, axis=0)
+
+
+rasterize(LGres, greenCards, "b")
 
 #rasterize(LBres, blueCards)
 
 #rasterize(DGres, darkGreenCards)
 
-rasterize(GRres, yellowCards)
+#rasterize(GRres, yellowCards)
 
 cv2.imshow('board', board)
 cv2.imshow('hsv', hsv)
